62bb47f5c6398adebbaa4d0c01d47b6fd7d7e7d8
Shaun Malone <berkano@gmail.com>
2014-10-17 12:48:37 +0200

    fix up launcher

--- core/src/katsu/GameDesktop.java
+++ core/src/katsu/GameDesktop.java
@@ -1,19 +1,18 @@
 package katsu;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 public abstract class GameDesktop {
 	public static void main(String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = Settings.hres;
-        config.height = Settings.vres;
-        config.foregroundFPS = Settings.targetFrameRate;
-        config.backgroundFPS = Settings.targetFrameRate;
-        config.vSyncEnabled = Settings.vsync;
-        config.x = 0;
-        config.y = 0;
-		new LwjglApplication(new Game(), config);
+
+        Porting.makeCompatible();
+//		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//        config.width = Settings.hres;
+//        config.height = Settings.vres;
+//        config.foregroundFPS = Settings.targetFrameRate;
+//        config.backgroundFPS = Settings.targetFrameRate;
+//        config.vSyncEnabled = Settings.vsync;
+//        config.x = 0;
+//        config.y = 0;
+//		new LwjglApplication(new Game(), config);
 	}
 
 
--- desktop/src/net/berkanoid/katsu/desktop/DesktopLauncher.java
+++ desktop/src/net/berkanoid/katsu/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package net.berkanoid.katsu.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import katsu.Game;
 import net.berkanoid.katsu.KatsuGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new KatsuGame(), config);
+		new LwjglApplication(new Game(), config);
 	}
 }
