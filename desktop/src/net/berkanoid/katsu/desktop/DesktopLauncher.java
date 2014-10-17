5ba1f4338973e1a74962b95c5eedbb6638c07a61
Shaun Malone <berkano@gmail.com>
2014-10-17 12:51:22 +0200

    fix up launcher

--- desktop/src/net/berkanoid/katsu/desktop/DesktopLauncher.java
+++ desktop/src/net/berkanoid/katsu/desktop/DesktopLauncher.java
@@ -3,11 +3,21 @@ package net.berkanoid.katsu.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import katsu.Game;
+import katsu.Settings;
 import net.berkanoid.katsu.KatsuGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+
+        config.width = Settings.hres;
+        config.height = Settings.vres;
+        config.foregroundFPS = Settings.targetFrameRate;
+        config.backgroundFPS = Settings.targetFrameRate;
+        config.vSyncEnabled = Settings.vsync;
+        config.x = 0;
+        config.y = 0;
+
+        new LwjglApplication(new Game(), config);
 	}
 }
