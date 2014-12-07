c2e2157da50a046e98f1cc6156553809269c13c6
Shaun Malone <berkano@gmail.com>
2014-12-07 23:02:44 +0000

    tuning

--- core/src/ld31v2/Tuning.java
+++ core/src/ld31v2/Tuning.java
@@ -8,8 +8,8 @@ public class Tuning {
     public static final int checkNeighbourCombatInterval = 500;
     public static final int pathFindInterval = 250;
     public static final int aiInterval = 2500;
-    public static final int maxSpawnInterval = 10000;
-    public static final int majorityTimeNeededForSpawn = 5000;
+    public static final int maxSpawnInterval = 7000;
+    public static final int majorityTimeNeededForSpawn = 3500;
     public static final int tryToGetUnstuck = 5000;
     public static int dieAfterStuck = 10000;
 
