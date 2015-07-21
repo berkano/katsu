be5dbf1db76840e0cd7548abd577f2236aaf4b76
Shaun Malone <berkano@gmail.com>
2015-07-21 19:37:15 +0100

    use spatial index to optimise grid based collision detection

--- core/src/katsu/KMovementConstrainer.java
+++ core/src/katsu/KMovementConstrainer.java
@@ -2,6 +2,8 @@ package katsu;
 
 import com.badlogic.gdx.math.Rectangle;
 
+import java.util.List;
+
 /**
  * Created by shaun on 16/11/2014.
  */
@@ -18,20 +20,17 @@ public class KMovementConstrainer {
 
         if (entity.isSolid()) {
 
-            // First get the room
-            KRoom room = entity.getRoom();
+            net.sf.jsi.Rectangle newRect = new net.sf.jsi.Rectangle(newX, newY, newX + entity.getWidth() - 1, newY - entity.getHeight() + 1);
+            List<KEntity> overlappingEntities = entity.getRoom().spatialSearchByIntersection(newRect);
 
             // Get all possible collision targets
-            for (KEntity other : room.getEntities()) {
+            for (KEntity other : overlappingEntities) {
                 if (other.isSolid() && other.canCollideWith(entity.getClass()) && entity.canCollideWith(other.getClass())) {
-                    if (entitiesWouldOverlap(entity, newX, newY, other)) {
                         couldMove = false;
                         entity.onCollide(other);
                         other.onCollide(entity);
-                    }
                 }
             }
-
         }
 
         if (couldMove) {
@@ -42,22 +41,6 @@ public class KMovementConstrainer {
         }
 
         return couldMove;
-
-    }
-
-    private static boolean entitiesWouldOverlap(KEntity entity, int newX, int newY, KEntity other) {
-
-        // Quick optimisation for obvious cases
-        if (newX > entity.getX() + other.getWidth()) return false;
-        if (newY > entity.getY() + other.getHeight()) return false;
-        if (newX < entity.getX() - other.getWidth()) return false;
-        if (newY < entity.getY() - other.getHeight()) return false;
-
-        // Bounding box intersection calculation
-        Rectangle r1 = new Rectangle(newX, newY, entity.getWidth(), entity.getHeight());
-        Rectangle r2 = new Rectangle(other.getX(), other.getY(), other.getWidth(), other.getHeight());
-
-        return r1.overlaps(r2);
 
     }
 }
