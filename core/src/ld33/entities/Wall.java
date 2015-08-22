4d96fc62699064511839076429628995d18332a3
Shaun Malone <shaun.malone@axonvibe.com>
2015-08-22 15:43:19 +0100

    tweak wall gfx

--- core/src/ld33/entities/Wall.java
+++ core/src/ld33/entities/Wall.java
@@ -1,5 +1,6 @@
 package ld33.entities;
 
+import katsu.K;
 import katsu.KEntityBase;
 
 /**
@@ -10,5 +11,8 @@ public class Wall extends KEntityBase {
     public Wall() {
         super();
         this.setSolid(true);
+        this.setSpriteRotation(K.random.nextInt(4) * 90 + K.random.nextInt(6) - 3);
+        this.setSpriteScale(K.random.nextFloat() * 0.1f + 1.0f);
+
     }
 }
