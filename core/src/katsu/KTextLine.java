72107097b4eeb483124a16c2b5f72140d1f7ab14
shaun <berkano@gmail.com>
2016-07-31 12:33:12 +0100

    tidy up

--- core/src/katsu/KTextLine.java
+++ core/src/katsu/KTextLine.java
@@ -1,12 +1,5 @@
 package katsu;
 
-/**
- * Created with IntelliJ IDEA.
- * User: shaun
- * Date: 13/04/13
- * Time: 19:24
- * To change this template use File | Settings | File Templates.
- */
 public class KTextLine {
 
     public long added;
