d6d103769dff6a080947aa45d52951e3413c9c5a
shaun <berkano@gmail.com>
2017-04-21 21:53:53 +0100

    silly bit of zooming/rotating

--- core/src/ld38/entities/Player.java
+++ core/src/ld38/entities/Player.java
@@ -1,5 +1,7 @@
 package ld38.entities;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import katsu.K;
 import katsu.KEntity;
 import katsu.KTiledMapEntity;
 
@@ -8,4 +10,16 @@ import katsu.KTiledMapEntity;
  */
 @KTiledMapEntity
 public class Player extends KEntity {
+
+    float zoom = 0.2f;
+
+    @Override
+    public void update() {
+        super.update();
+        lookAtMe();
+        OrthographicCamera camera = K.graphics.camera;
+        zoom += 0.001f;
+        camera.zoom = zoom;
+        camera.rotate(0.03f);
+    }
 }
