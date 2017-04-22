3e364ecab5a1f87efe83f0771711db62ea5acf3b
shaun <berkano@gmail.com>
2017-04-22 10:06:48 +0100

    random troll names

--- core/src/ld38/TrollNamer.java
+++ core/src/ld38/TrollNamer.java
@@ -1,11 +1,35 @@
 package ld38;
 
+import katsu.K;
+
+import java.util.ArrayList;
+
 /**
  * Created by shaun on 22/04/2017.
  */
 public class TrollNamer {
 
     private static TrollNamer _instance;
+
+    private ArrayList<String> availableNames = new ArrayList<String>();
+
+    public TrollNamer() {
+        generateNames();
+    }
+
+    private void generateNames() {
+
+        availableNames.add("Bob1");
+        availableNames.add("Bob2");
+        availableNames.add("Bob3");
+        availableNames.add("Bob4");
+        availableNames.add("Bob5");
+        availableNames.add("Bob6");
+        availableNames.add("Bob7");
+        availableNames.add("Bob8");
+        availableNames.add("Bob9");
+
+    }
 
     public static TrollNamer instance() {
         if (_instance == null) {
@@ -15,6 +39,9 @@ public class TrollNamer {
     }
 
     public String nextName() {
-        return "Bob";
+        int pick = K.random.nextInt(availableNames.size());
+        String result = availableNames.get(pick);
+        availableNames.remove(pick);
+        return result;
     }
 }
