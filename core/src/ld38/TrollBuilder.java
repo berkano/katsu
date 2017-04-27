3bbc20c1a2ef96b32d0fd2772ba85c7e171b1529
shaun <berkano@gmail.com>
2017-04-27 07:40:26 +0100

    code metrics/refactoring - TrollBuilder

--- core/src/ld38/TrollBuilder.java
+++ core/src/ld38/TrollBuilder.java
@@ -60,44 +60,7 @@ public class TrollBuilder {
         }
 
         if (!foundTower) {
-
-            if (game.wallsBuilt < 16) {
-                if (!DevHelper.skipWallRule) {
-                    selectedTroll.say("[RED]build more walls!");
-                    return;
-                }
-            }
-
-            int x = selectedTroll.getX();
-            int y = selectedTroll.getY();
-
-            logger.info("Attempting to build tower at precise position " + x + "," + y);
-            // 144,112
-            // 208,112
-            // 208,48
-            // 144,48
-            boolean allowedTowerLocation = false;
-            if (x == 144 && y == 112) allowedTowerLocation = true;
-            if (x == 208 && y == 112) allowedTowerLocation = true;
-            if (x == 208 && y == 48) allowedTowerLocation = true;
-            if (x == 144 && y == 48) allowedTowerLocation = true;
-
-            if (!allowedTowerLocation) {
-                selectedTroll.say("[RED]no build tower here!");
-                return;
-            }
-
-            if (game.stone < 10) {
-                selectedTroll.say("[RED]need 10 stone!");
-                return;
-            }
-            game.stone -= 10;
-            Tower tower = new Tower();
-            tower.setX(selectedTroll.getX());
-            tower.setY(selectedTroll.getY());
-            room.addNewEntity(tower);
-            game.build.play();
-            selectedTroll.say("built tower!");
+            buildTower();
             return;
         }
 
@@ -124,6 +87,50 @@ public class TrollBuilder {
         }
 
         selectedTroll.say("[RED]is gold tower already!");
+
+    }
+
+    private void buildTower() {
+
+        Troll troll = manager.getSelectedTroll();
+
+        if (game.wallsBuilt < 16) {
+            if (!DevHelper.skipWallRule) {
+                troll.say("[RED]build more walls!");
+                return;
+            }
+        }
+
+        int x = troll.getX();
+        int y = troll.getY();
+
+        logger.info("Attempting to build tower at precise position " + x + "," + y);
+        // 144,112
+        // 208,112
+        // 208,48
+        // 144,48
+        boolean allowedTowerLocation = false;
+        if (x == 144 && y == 112) allowedTowerLocation = true;
+        if (x == 208 && y == 112) allowedTowerLocation = true;
+        if (x == 208 && y == 48) allowedTowerLocation = true;
+        if (x == 144 && y == 48) allowedTowerLocation = true;
+
+        if (!allowedTowerLocation) {
+            troll.say("[RED]no build tower here!");
+            return;
+        }
+
+        if (game.stone < 10) {
+            troll.say("[RED]need 10 stone!");
+            return;
+        }
+        game.stone -= 10;
+        Tower tower = new Tower();
+        tower.setX(troll.getX());
+        tower.setY(troll.getY());
+        room.addNewEntity(tower);
+        game.build.play();
+        troll.say("built tower!");
 
     }
 
