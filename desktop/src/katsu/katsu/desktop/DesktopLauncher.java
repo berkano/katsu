08b1353386f40697ece5d34290d2c1ca53bc98f1
shaun <berkano@gmail.com>
2021-03-14 08:46:31 +0000

    fresh libgdx

---
+++ desktop/src/katsu/katsu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package katsu.katsu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import katsu.katsu.KLauncher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KLauncher(), config);
+	}
+}
